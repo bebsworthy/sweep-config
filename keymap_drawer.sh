2cb17e7a2ae68bf92ab9f08d060c1f915f674a72
boyd <bebsworthy@gmail.com>
2024-08-18 14:36:39 +0200

    cleanup

--- keymap_drawer.sh
+++ keymap_drawer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-keymap -c keymap_drawer_config.yaml parse -c 10 -z ./config/splitkb_aurora_sweep.keymap > keymap_drawer.yaml
-keymap -c keymap_drawer_config.yaml draw keymap_drawer.yaml > keymap.svg
+keymap -c keymap_drawer_config.yaml parse -c 10 -z ./config/splitkb_aurora_sweep.keymap > keymap-drawer/splitkb_aurora_sweep.yaml
+keymap -c keymap_drawer_config.yaml draw keymap-drawer/splitkb_aurora_sweep.yaml > keymap-drawer/splitkb_aurora_sweep.svg
